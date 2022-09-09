c16af9426c386e210db8a48031f112b2cb8490d6
Fred Jiang <jiangf@shinetechsoftware.com>
2022-09-09 14:30:16 +0800

    init

--- App.tsx
+++ App.tsx
@@ -1,18 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, useColorScheme, View } from 'react-native';
-import { PlayDemo } from './app/components/PlayDemo';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const colorScheme = useColorScheme();
-  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
-  const themeContainerStyle =
-    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
-
   return (
-    <View style={[styles.container, themeContainerStyle]}>
-      <Text>Hello RN</Text>
-      <PlayDemo />
-      <Text style={[themeTextStyle]}>Color scheme: {colorScheme}</Text>
+    <View style={styles.container}>
+      <Text>Hello React Native!</Text>
       <StatusBar />
     </View>
   );
@@ -23,17 +15,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  lightContainer: {
-    backgroundColor: '#d0d0c0',
-  },
-  darkContainer: {
-    backgroundColor: '#242c40',
-  },
-  lightThemeText: {
-    color: '#242c40',
-  },
-  darkThemeText: {
-    color: '#d0d0c0',
   },
 });
