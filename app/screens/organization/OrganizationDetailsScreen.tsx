6784b17d5c34937360ec0a9c48e3cf846890e212
Fred Jiang <jiangf@shinetechchina.com>
2022-09-20 06:45:38 +0800

    hide parent header

--- app/screens/organization/OrganizationDetailsScreen.tsx
+++ app/screens/organization/OrganizationDetailsScreen.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { Text } from 'react-native';
 
-export default function OrganizationDetailsScreen() {
+export default function OrganizationDetailsScreen({ navigation, route }) {
+  useLayoutEffect(() => {
+    if (!navigation || !route) return
+
+    const parent = navigation.getParent()
+    if (parent) {
+      parent.setOptions({
+        headerShown: false,
+      })
+    }
+
+    return parent
+      ? () => {
+          parent.setOptions({
+            headerShown: true,
+          })
+        }
+      : undefined
+  }, [navigation, route])
+
   return <Text>Organization Details</Text>;
 }
