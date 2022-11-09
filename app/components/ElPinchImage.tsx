4137b165d08bc638b2505ccedc4353e7b59b9b56
Fred Jiang <jiangf@shinetechsoftware.com>
2022-11-09 17:49:36 +0800

    image gesture

--- app/components/ElPinchImage.tsx
+++ app/components/ElPinchImage.tsx
@@ -1,102 +1,71 @@
-import { Box } from 'native-base';
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import {
-  PinchGestureHandler,
-  PinchGestureHandlerStateChangeEvent,
-  RotationGestureHandler,
-  RotationGestureHandlerStateChangeEvent,
-  State,
-} from 'react-native-gesture-handler';
-import Animated from 'react-native-reanimated';
-
-const USE_NATIVE_DRIVER = true;
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 export default function ElPinchImage() {
-  const imagePinch = React.createRef();
-  const imageRotation = React.createRef();
-  let lastRotate = 0;
-  let lastScale = 1;
-  const rotate = new Animated.Value(0);
-  const pinchScale = new Animated.Value(1);
-  const baseScale = new Animated.Value(1);
-  const scale = Animated.multiply(baseScale, pinchScale);
-  const rotateStr = rotate.interpolate({
-    inputRange: [-100, 100],
-    outputRange: ['-100rad', '100rad'],
+  const position = useSharedValue({ x: 0, y: 0 });
+  const start = useSharedValue({ x: 0, y: 0 });
+  const scale = useSharedValue(1);
+  const savedScale = useSharedValue(1);
+  const rotation = useSharedValue(1);
+  const savedRotation = useSharedValue(1);
+
+  const panGesture = Gesture.Pan()
+    .onUpdate(e => {
+      position.value = {
+        x: e.translationX + start.value.x,
+        y: e.translationY + start.value.y,
+      };
+    })
+    .onEnd(() => {
+      start.value = {
+        x: position.value.x,
+        y: position.value.y,
+      };
+    });
+
+  const pinchGesture = Gesture.Pinch()
+    .onUpdate(e => {
+      scale.value = savedScale.value * e.scale;
+    })
+    .onEnd(() => {
+      savedScale.value = scale.value;
+    });
+
+  const rotationGesture = Gesture.Rotation()
+    .onUpdate(e => {
+      rotation.value = savedRotation.value + e.rotation;
+    })
+    .onEnd(() => {
+      savedRotation.value = rotation.value;
+    });
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: position.value.x },
+        { translateY: position.value.y },
+        { scale: scale.value },
+        { rotateZ: `${(rotation.value / Math.PI) * 180}deg` }
+      ],
+    };
   });
 
-  const tilt = new Animated.Value(0);
-  const tiltStr = tilt.interpolate({
-    inputRange: [-501, -500, 0, 1],
-    outputRange: ['1rad', '1rad', '0rad', '0rad'],
-  });
-
-  const onRotateGestureEvent = Animated.event(
-    [{ nativeEvent: { rotation: rotate } }],
-    { useNativeDriver: USE_NATIVE_DRIVER },
-  );
-
-  const onPinchGestureEvent = Animated.event(
-    [{ nativeEvent: { scale: pinchScale } }],
-    { useNativeDriver: USE_NATIVE_DRIVER },
-  );
-
-  const onRotateHandlerStateChange = (
-    event: RotationGestureHandlerStateChangeEvent,
-  ) => {
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      lastRotate += event.nativeEvent.rotation;
-      rotate.setOffset(lastRotate);
-      rotate.setValue(0);
-    }
-  };
-
-  const onPinchHandlerStateChange = (
-    event: PinchGestureHandlerStateChangeEvent,
-  ) => {
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      lastScale *= event.nativeEvent.scale;
-      baseScale.setValue(lastScale);
-      pinchScale.setValue(1);
-    }
-  };
+  const composed = Gesture.Race(panGesture, pinchGesture, rotationGesture);
 
   return (
-    <>
-      <Box>123</Box>
-      <Animated.Image source={require('./mario-av.png')} />
-      <RotationGestureHandler
-        ref={imageRotation}
-        simultaneousHandlers={imagePinch}
-        onGestureEvent={onRotateGestureEvent}
-        onHandlerStateChange={onRotateHandlerStateChange}
-      >
-        <Animated.View style={styles.container} collapsable={false}>
-          <PinchGestureHandler
-            ref={imagePinch}
-            simultaneousHandlers={imageRotation}
-            onGestureEvent={onPinchGestureEvent}
-            onHandlerStateChange={onPinchHandlerStateChange}
-          >
-            <Animated.Image
-              style={[
-                styles.pinchableImage,
-                {
-                  transform: [
-                    { perspective: 200 },
-                    { scale: scale },
-                    { rotate: rotateStr },
-                    { rotateX: tiltStr },
-                  ],
-                },
-              ]}
-              source={require('./mario-av.png')}
-            />
-          </PinchGestureHandler>
-        </Animated.View>
-      </RotationGestureHandler>
-    </>
+    <Animated.View style={styles.container} collapsable={false}>
+      <GestureDetector gesture={composed}>
+        <Animated.Image
+          style={[styles.pinchableImage, animatedStyles]}
+          source={require('./mario-av.png')}
+        />
+      </GestureDetector>
+    </Animated.View>
   );
 }
 
